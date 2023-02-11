43e10114c425c703811098acbacdf58b679923c7
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-02-11 19:30:26 +0600

    Changed tab size in prettierc and Added DeteleTodos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,115 +4,115 @@ import "../App.css";
 import "../reset.css";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "Finish React Series",
-      isComplete: false,
-    },
-    {
-      id: 2,
-      title: "Go to Grocery",
-      isComplete: true,
-    },
-    {
-      id: 3,
-      title: "Do other thing",
-      isComplete: false,
-    },
-  ]);
-
-  const [todoInput, setTodoInput] = useState("");
-
-  function addTodo(event) {
-    event.preventDefault();
-
-    if (todoInput.trim().length === 0) return;
-
-    setTodos([
-      ...todos,
-      {
-        id: Date.now(),
-        title: todoInput,
-        isComplete: false,
-      },
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            title: "Finish React Series",
+            isComplete: false,
+        },
+        {
+            id: 2,
+            title: "Go to Grocery",
+            isComplete: true,
+        },
+        {
+            id: 3,
+            title: "Do other thing",
+            isComplete: false,
+        },
     ]);
 
-    setTodoInput("");
-  }
+    const [todoInput, setTodoInput] = useState("");
 
-  const getTodoInput = (event) => setTodoInput(event.target.value);
+    function addTodo(event) {
+        event.preventDefault();
 
-  return (
-    <div className="todo-app-container">
-      <div className="todo-app">
-        <h2>Todo App</h2>
-        <form action="#" onSubmit={addTodo}>
-          <input
-            type="text"
-            className="todo-input"
-            placeholder="What do you need to do?"
-            value={todoInput}
-            onChange={getTodoInput}
-          />
-        </form>
+        if (todoInput.trim().length === 0) return;
 
-        <ul className="todo-list">
-          {todos.map((todo) => (
-            <li key={todo.id} className="todo-item-container">
-              <div className="todo-item">
-                <input type="checkbox" />
-                <span
-                  className={`todo-item-label ${
-                    todo.isComplete ? "line-through" : ""
-                  }`}
-                >
-                  {todo.title}
-                </span>
-                {/* <input type="text" className="todo-item-input" value="Go to Grocery" /> */}
-              </div>
-              <button className="x-button">
-                <svg
-                  className="x-button-icon"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </li>
-          ))}
-        </ul>
+        setTodos([
+            ...todos,
+            {
+                id: Date.now(),
+                title: todoInput,
+                isComplete: false,
+            },
+        ]);
 
-        <div className="check-all-container">
-          <div>
-            <div className="button">Check All</div>
-          </div>
+        setTodoInput("");
+    }
 
-          <span>3 items remaining</span>
+    const deleteTodo = (id) => setTodos([...todos].filter((todo) => todo.id != id));
+
+    const getTodoInput = (event) => setTodoInput(event.target.value);
+
+    return (
+        <div className="todo-app-container">
+            <div className="todo-app">
+                <h2>Todo App</h2>
+                <form action="#" onSubmit={addTodo}>
+                    <input
+                        type="text"
+                        className="todo-input"
+                        placeholder="What do you need to do?"
+                        value={todoInput}
+                        onChange={getTodoInput}
+                    />
+                </form>
+
+                <ul className="todo-list">
+                    {todos.map((todo) => (
+                        <li key={todo.id} className="todo-item-container">
+                            <div className="todo-item">
+                                <input type="checkbox" />
+                                <span
+                                    className={`todo-item-label ${
+                                        todo.isComplete ? "line-through" : ""
+                                    }`}
+                                >
+                                    {todo.title}
+                                </span>
+                                {/* <input type="text" className="todo-item-input" value="Go to Grocery" /> */}
+                            </div>
+                            <button className="x-button" onClick={() => deleteTodo(todo.id)}>
+                                <svg
+                                    className="x-button-icon"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+
+                <div className="check-all-container">
+                    <div>
+                        <div className="button">Check All</div>
+                    </div>
+
+                    <span>3 items remaining</span>
+                </div>
+
+                <div className="other-buttons-container">
+                    <div>
+                        <button className="button filter-button filter-button-active">All</button>
+                        <button className="button filter-button">Active</button>
+                        <button className="button filter-button">Completed</button>
+                    </div>
+                    <div>
+                        <button className="button">Clear completed</button>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div className="other-buttons-container">
-          <div>
-            <button className="button filter-button filter-button-active">
-              All
-            </button>
-            <button className="button filter-button">Active</button>
-            <button className="button filter-button">Completed</button>
-          </div>
-          <div>
-            <button className="button">Clear completed</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
